7e373d160eb2b154f7aa4771720024368a5ee017
nvanderp <nvanderp@gmail.com>
2017-11-13 10:56:24 -0500

    db models working!

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
+// OUR MODELS
+const models = require('./models');
+const Page = models.Page;
+const User = models.User;
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
@@ -19,9 +23,10 @@ app.use(bodyParser.json());
 var testRoutes = require('./routes/index.js');
 app.use('/', testRoutes);
 
-app.listen(1337, function() {
-  console.log('Listening on 1337');
-});
-
-
-
+models.db.sync({force: true})
+.then(function() {
+  app.listen(3000, function() {
+    console.log('Server is listening on port 3000!');
+  });
+})
+.catch(console.error);
---
+++ models/index.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
+
+const Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING
+    },
+    urlTitle: {
+        type: Sequelize.STRING
+    },
+    content: {
+        type: Sequelize.TEXT
+    },
+    status: {
+        type: Sequelize.BOOLEAN // true = open; false = closed
+    }
+});
+
+const User = db.define('user', {
+    name: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+module.exports = {
+  Page: Page,
+  User: User,
+  db: db
+};
